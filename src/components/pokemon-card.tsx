a69871b6bf768f2491aa9556fae6044e03c6d793
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-09-23 21:47:15 -0500

    feat: consume of api

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 export function PokemonCard(props: {name: string, url: string}) {
-    return <div>
-        {props.name} {props.url}
-    </div>
+    const {name, url} = props;
+    const pokemonId = url.slice(0, -1).split("/").pop()
+
+    return (
+        <div className="card border bg-base-100 shadow-xl col-span-4">
+            <figure>
+                <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
+                    alt={name} 
+                    className="size-[200px]"
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name.charAt(0).toUpperCase()+name.slice(1)}</h2>
+                <p>Pokemon ID: {pokemonId}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Ver Pokemon</button>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/components/pokemon-list.tsx
+++ src/components/pokemon-list.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { PokemonCard } from "./pokemon-card";
 
 type Pokemon = {
@@ -5,49 +6,23 @@ type Pokemon = {
     url: string;
 };
 
-const pokemons: Pokemon[] = [
-    {
-      name: "bulbasaur",
-      url: "https://pokeapi.co/api/v2/pokemon/1/",
-    },
-    {
-      name: "ivysaur",
-      url: "https://pokeapi.co/api/v2/pokemon/2/",
-    },
-    {
-      name: "venusaur",
-      url: "https://pokeapi.co/api/v2/pokemon/3/",
-    },
-    {
-      name: "charmander",
-      url: "https://pokeapi.co/api/v2/pokemon/4/",
-    },
-    {
-      name: "charmeleon",
-      url: "https://pokeapi.co/api/v2/pokemon/5/",
-    },
-    {
-      name: "charizard",
-      url: "https://pokeapi.co/api/v2/pokemon/6/",
-    },
-    {
-      name: "squirtle",
-      url: "https://pokeapi.co/api/v2/pokemon/7/",
-    },
-    {
-      name: "wartortle",
-      url: "https://pokeapi.co/api/v2/pokemon/8/",
-    },
-    {
-      name: "blastoise",
-      url: "https://pokeapi.co/api/v2/pokemon/9/",
-    },
-  ];
-
 export  const PokemonList = () => {
-    return <div>
+
+  const [pokemons, setpokemons] = useState<Pokemon[]>([])
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/").then((response)=> 
+      response.json().then((data) => {
+        console.log(data)
+        setpokemons(data.results)
+      })
+    )
+  }, [])
+
+    return (
+      <div className="grid grid-cols-12 gap-4 mt-8">
         {pokemons.map((pokemon) => (
             <PokemonCard key={pokemon.url} name={pokemon.name} url={pokemon.url}/>
         ))}
         </div>
+    )
 }
